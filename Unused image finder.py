f37993b3d2168105ab3b1c3d65da1ac97aaa3592
Calvin L <47407549+Coderdude112@users.noreply.github.com>
2020-11-10 11:09:04 -0700

    Update Unused image finder.py

--- Unused image finder.py
+++ Unused image finder.py
@@ -9,43 +9,46 @@ from win10toast import ToastNotifier
 n = ToastNotifier()
 import configparser
 config = configparser.ConfigParser()
-config.read(os.getcwd() + "\\Auto tools\\variables.ini")    # Read the Variables.ini file
+config.read(os.getcwd() + "\\Auto tools\\variables.ini")    # Read the variables.ini file
 
-# Set all variables
-alert_file = config['Manual_variables']['alert_file']
+# Define all variables, arrays, ect
+alert_file = config['Global']['alert_file']
 uif_path = config['Unused_image_finder']['uif_path']
 uif_file_type = config['Unused_image_finder']['uif_file_type']
 uif_image_path = config['Unused_image_finder']['uif_image_path']
+files = []              # An array with all the files in uif_path
+images = []             # An array with all the files in uif_image_path regaurdless of their extention
+need_to_alert = False   # Do we need to alert the user of something
 
 def get_files():
     global files
-    files = []
 
-    for r, d, f in os.walk(uif_path):   # Get all the files recursively in the 'uif_path' path (r=root, d=directories, f = files)
+    for r, d, f in os.walk(uif_path): # Get all the files recursively in the uif_path (r=root, d=directories, f = files)
         for file in f:  # Just get the filenames
             if uif_file_type in file:   # Filter out files that don't match the extention
                 files.append(os.path.join(r, file)) # Add the files to the array 'files'
 def get_images():
     global images
-    images = []
 
     for r, d, f in os.walk(uif_image_path):   # Get all the files recursively in the 'uif_image_path' path (r=root, d=directories, f = files)
         for file in f:  # Just get the filenames
             images.append(file) # Add the images to the array 'images'
-def unused_images_alert():
-    for i in images:
-        with open(alert_file, "a") as alert_file_data:   # For all the unused images
-            alert_file_data.write("The image \"" + i + "\" is currently unused.\n\n")
-            n.show_toast("Unused Image", "The image " + i + " is unused.\n Check the ALERT file", icon_path=os.getcwd() + "\\Auto tools\\Alert.ico", duration=10)
 
 get_files()
 get_images()
 
-for i in files: # For all the items within the 'files' array
-    with open(i) as open_file:
-        file_data = open_file.read() # Read the file into the variable 'file_data'
-        for image in images:
-            if image in file_data:  # Check if the images are in 'file_data'
-                images.remove(image)    # The image is found, so remove it
+for i in files: # For all the items within the files array
+    current_file_contence = open(i).read
+    for j in images:    # For all the items in the array images
+        if j in current_file_contence:
+            images.remove(j)    # The image is used at least once, so remove it from the images array
 
-unused_images_alert()   # Alert to the unused images
+for i in images:    # Add a notice in the alert_file for all the unused images left in images
+    global need_to_alert
+
+    need_to_alert = True
+    open(alert_file, "a").write("The image \"" + i + "\" is currently unused.\n")
+
+if need_to_alert == True:
+    n.show_toast("Unused Image", "At least one unused image has been found.\nCheck the ALERT file", icon_path=os.getcwd() + "\\Auto tools\\Alert.ico", duration=10)
+    open(alert_file, "a").write("\n")
