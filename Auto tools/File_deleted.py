8ef806e46fd9fb8b55f76a5c1b8f74b49129171c
Calvin L <47407549+Coderdude112@users.noreply.github.com>
2020-12-22 19:16:51 -0700

    Update File_deleted.py

--- Auto tools/File_deleted.py
+++ Auto tools/File_deleted.py
@@ -1,58 +1,27 @@
-#############################################################################
-# File_deleted.py                                                           #
-# Run by Link_helper.py to determine weather a file was deleted or moved    #
-#############################################################################
+#################################################################################
+# File_deleted.py                                                               #
+# Run by Link_helper.py to determine weather a file was deleted or moved dirs   #
+#################################################################################
 
 # Import and configure all plugins
-import time, os
-from win10toast import ToastNotifier
-n = ToastNotifier() 
-import configparser
-config = configparser.ConfigParser()
+import time, os, configparser, sys
+ini_file = configparser.ConfigParser()
 
-# Wait a second so we can really dertime if a file was deleted or just moved, then read the variables.ini file
-time.sleep(1)
-config.read(os.getcwd() + "\\variables.ini")    # Read the variables.ini file
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir)))  # Add the parent directory to the PATH so we can import our functions
+import Functions as mdf # Import all functions within Functions.py as mdf
+
+time.sleep(1)   # Wait a second to let Link_helper.py update variables.ini if it needs to
 
 # Define all variables, arrays, ect
-working_path = config['Global']['working_path']
-file_type = config['Global']['file_type']
-alert_file = config['Global']['alert_file']
-deleted = config['Auto_tools_variables']['deleted']
-old_link = config['Auto_tools_variables']['old_link']
-new_link = config['Auto_tools_variables']['new_link']
-files = []  # An array with all of the files notebook
-
-def get_files():
-    global files
-
-    for r, d, f in os.walk(working_path): # Get all the files recursively in the notebook (r=root, d=directories, f = files)
-        for file in f:  # Just get the filenames
-            if file_type in file:   # Filter out files that don't match the extention
-                files.append(os.path.join(r, file)) # Add the files to the array 'files'
-def search_and_replace():
-    get_files() # Get the files in the notebooks folder
-        
-    for i in files: # For all the files in the list replace the old_link with the new_link
-        current_file_contence = open(i).read()  # Open the file
-        current_file_contence = current_file_contence.replace(old_link, new_link)   # Replace old_link with new_link
-        open(i, "wt").write(current_file_contence)  # Write the current_file_contence to the file
-def search_and_alert():
-    get_files()
-
-    for i in files: # For all the files in the files array search and alert
-        current_file_contence = open(i).read()
-        if old_link in current_file_contence:
-            global need_to_alert
-
-            need_to_alert = True
-            open(alert_file, "a").write("The file *" + os.path.basename(old_link) + "* was deleted, but the file **[" + os.path.basename(i) + "](" + i + ")** still has a link to the deleted file.\n")
+ini_file.read(os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + "\\variables.ini") # Read the variables.ini file up one directory
+deleted = ini_file['Auto_tools_variables-do_NOT_edit']['deleted']   # If this is "yes" then a file was deleted. If this is "no" then a file was moved dirs
+notebook_dir = ini_file['Globals']['notebook_dir']                  # The base directory of the notebook
+file_type = ini_file['Globals']['file_type']                        # The file extension of the files that make-up the notebook (This is usually .md for markdown notebooks)
+old_link = ini_file['Auto_tools_variables-do_NOT_edit']['old_link'] # The old link that will be replaced by the new_link
+new_link = ini_file['Auto_tools_variables-do_NOT_edit']['new_link'] # The new link that will replace the old_link
+alert_file = ini_file['Globals']['alert_file']                      # The file to use when making detailed alerts to the user
 
 if deleted == "no": # The file was moved, not deleted
-    search_and_replace()
-elif deleted == "yes":  # The file was deleted
-    search_and_alert()
-
-if need_to_alert == True:   # A broken link has been created due to the file being deleted so we need to alert the user
-    n.show_toast("Broken Link", "Due to " + os.path.basename(old_link) + " being deleted a broken link has been created.\nCheck the ALERT file", icon_path=os.getcwd() + "\\Alert.ico", duration=10)
-    open(alert_file, "a").write("\n")
+    mdf.search_and_replace(notebook_dir, file_type, old_link, new_link)
+elif deleted == "yes":  # The file was deleted, not moved
+    mdf.search_and_alert(notebook_dir, file_type, old_link, 'Broken link due to deletion', alert_file, 'Broken link due to deletion')
